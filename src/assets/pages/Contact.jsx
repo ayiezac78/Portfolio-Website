ecf1b02a994c9ea164db0ad01e6ebb9653943a69
Ariel Maniago <admaniago01@gmail.com>
2023-05-02 17:37:26 +0800

    official portfolio

--- src/assets/pages/Contact.jsx
+++ src/assets/pages/Contact.jsx
@@ -20,7 +20,7 @@ const Contact = () => {
         <div className="max-w-md">
           <h1 className="text-5xl font-bold">Let's Connect</h1>
           <p className="py-6">
-            If you have any questions or just want to say hello, please don't hesitate to reach out. I will make an effort to respond as soon as possible.
+            If you have any suggestion, questions or just want to say hello, please don't hesitate to reach out. I will make an effort to respond as soon as possible.
           </p>
           <a href='mailto:admaniago01@gmail.com' className="btn btn-active dark:btn-outline btn-ghost outline outline-2 text-[#324B4C] gap-2">Send a message<BiMailSend className='inline-block h-6 w-6'/></a>
         </div>
